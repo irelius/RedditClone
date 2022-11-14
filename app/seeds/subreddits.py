46216a9e5013288d05dcfbe539354fbb8a9bf1ae
irelius <kihong711@hotmail.com>
2022-11-13 18:38:23 -0800

    subreddit seeder file created - 6:38 PM, 11/13/22

---
+++ app/seeds/subreddits.py
@@ -0,0 +1,36 @@
+from app.models import db, Subreddit
+
+def seed_subreddits():
+    subreddit_one = Subreddit(
+        name = "Subreddit 1",
+        description = "Subreddit 1 description"
+    )
+    subreddit_two = Subreddit(
+        name = "Subreddit 2",
+        description = "Subreddit 2 description"
+    )
+    subreddit_three = Subreddit(
+        name = "Subreddit 3",
+        description = "Subreddit 3 description"
+    )
+    subreddit_four = Subreddit(
+        name = "Subreddit 4",
+        description = "Subreddit 4 description"
+    )
+    subreddit_five = Subreddit(
+        name = "Subreddit 5",
+        description = "Subreddit 5 description"
+    )
+
+    db.session.add(subreddit_one)
+    db.session.add(subreddit_two)
+    db.session.add(subreddit_three)
+    db.session.add(subreddit_four)
+    db.session.add(subreddit_five)
+
+
+    db.session.commit()
+
+def undo_subreddits():
+    db.session.execute("DELETE FROM subreddits;")
+    db.session.commit()
