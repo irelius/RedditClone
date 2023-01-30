2619cc1af589a3933c68d8fee5533fb35679644b
irelius <kihong711@hotmail.com>
2023-01-29 23:55:39 -0800

    test

---
+++ migrations/versions/c71a5861d267_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: c71a5861d267
+Revises: 2cf2155fd441
+Create Date: 2023-01-29 23:54:20.407636
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'c71a5861d267'
+down_revision = '2cf2155fd441'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
