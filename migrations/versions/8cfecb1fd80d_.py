a777ae5a02513b7d21635d09396c5476ca50ccac
irelius <kihong711@hotmail.com>
2023-01-30 15:51:53 -0800

    fixing migration file?

---
+++ migrations/versions/8cfecb1fd80d_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: 8cfecb1fd80d
+Revises: 229f5dd383a9
+Create Date: 2023-01-30 15:51:28.741583
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '8cfecb1fd80d'
+down_revision = '229f5dd383a9'
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
