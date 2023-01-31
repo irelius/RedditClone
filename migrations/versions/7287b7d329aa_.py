74a5b8ac16678a74bf5cac779600fc667dd094a2
irelius <kihong711@hotmail.com>
2023-01-30 18:07:19 -0800

    forgot to import for migration

--- migrations/versions/7287b7d329aa_.py
+++ migrations/versions/7287b7d329aa_.py
@@ -8,6 +8,9 @@ Create Date: 2023-01-30 16:07:31.711154
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
 
 # revision identifiers, used by Alembic.
 revision = '7287b7d329aa'
